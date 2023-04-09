601fa8514bff037a8042f594aa8e1c8d063a2b1e
sakibmahbub <arafatsakib48@gmail.com>
2023-04-09 21:57:32 +0600

    added home & about page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import ErrorPage from "./components/ErrorPage";
+import Shop from "./components/Shop";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
