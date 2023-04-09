1430be7f01812c3c4637c8a23254c333c8bc0396
sakibmahbub <arafatsakib48@gmail.com>
2023-04-10 01:31:40 +0600

    displayed products in card

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { getStoredCart } from "../utils/fakeDB";
+import { useLoaderData } from "react-router-dom";
+import CartItem from "./Cards/CartItem";
 
 const Cart = () => {
-  const cart = getStoredCart();
-  console.log(cart);
-  return <div></div>;
+  const { initialCart } = useLoaderData();
+
+  return (
+    <div className="flex min-h-screen items-start justify-center bg-gray-100 text-gray-900">
+      <div className="flex flex-col max-w-3xl p-6 space-y-4 sm:p-10">
+        <h2 className="text-xl font-semibold">
+          {initialCart.length ? "Review Cart Items" : "Cart Is Empty"}
+        </h2>
+        <ul className="flex flex-col divide-y divide-gray-700">
+          {initialCart.map((product) => (
+            <CartItem key={product.id} product={product} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- src/loaders/getCart&ProductData.js
+++ src/loaders/getCart&ProductData.js
@@ -3,15 +3,17 @@ import { getStoredCart } from "../utils/fakeDB";
 export const productAndCartData = async () => {
   const productsData = await fetch("products.json");
   const products = await productsData.json();
-  const savedCart = getStoredCart();
 
-  let cartArr = [];
+  const savedCart = getStoredCart();
+  const initialCart = [];
   for (const id in savedCart) {
     const foundProduct = products.find((product) => product.id === id);
     if (foundProduct) {
-      foundProduct.quantity = savedCart[id];
-      cartArr.push(foundProduct);
+      const quantity = savedCart[id];
+      foundProduct.quantity = quantity;
+      initialCart.push(foundProduct);
     }
   }
-  return { cartArr, products };
+
+  return { products, initialCart };
 };
